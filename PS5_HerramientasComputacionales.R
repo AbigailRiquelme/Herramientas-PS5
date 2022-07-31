6e64a4c18b1cbb1a109d1ba7e9ee5cfa00416c40
AbigailRiquelme <anaabigailriquelme@gmail.com>
2022-07-31 17:38:54 -0300

    Cambios en el script

---
+++ PS5_HerramientasComputacionales.R
@@ -0,0 +1,29 @@
+#########################################
+#                                       #
+#     Herramientas computacionales      #
+#          para investigación           #
+#                                       #
+#       Profesora: Amelia Gibbons       #
+#      Alumnos: Pacheco y Riquelme      #
+#                                       #
+#           MAE UdeSA 2022              #
+#                                       #
+#########################################
+
+# Seteamos el directorio 
+
+
+#### Gráficos originales ### 
+
+## Gráfico 1 ##
+
+
+
+## Gráfico 2 ##
+
+
+
+## Gráfico 3 ##
+
+
+#### Gráficos corregidos ### 
