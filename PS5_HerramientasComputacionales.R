5b7154e89fdc43c867d3114451f7a9859b77c032
AbigailRiquelme <anaabigailriquelme@gmail.com>
2022-07-31 22:04:27 -0300

    Ultimos cambios en los graficos

--- PS5_HerramientasComputacionales.R
+++ PS5_HerramientasComputacionales.R
@@ -13,6 +13,13 @@
 # Seteamos el directorio 
 
 
+setwd("")
+
+# Verificamos el directorio
+
+getwd()
+
+
 # Definimos la paleta de colores 
 
 colores <- c("#00ABC5","#cfb0b4" ,"#ff3c84","#FF7F32", "#edf71c", "#941cf7")
@@ -27,43 +34,48 @@ colores2 <- c("#f5fa7b", "#3cffb7", "#ffdaaa", "#fa66f3",
 ## Gráfico 1 ##
 
 
-# original 
-
-#Activity B:Using faceting to understand data
-#"Aqu? quiere ver la distribución de las cantidades de los préstamos para diferentes grados de crédito.
-# Objetivo: Trazar el monto del préstamo para diferentes grados de crédito usando el faceting.
 df3s <- subset(df3,grade %in% c("A","B","C","D","E","F","G"))
 pb1<-ggplot(df3s,aes(x=loan_amnt))
 pb1
 pb2<-pb1+geom_density(bins=10,fill="cadetblue4")
 pb2
-#Facet_wrap
+
 pb3<-pb2+facet_wrap(~grade) 
 pb3
-#Free y coordinate for the subplots
+
 pb4<-pb3+geom_histogram() + facet_wrap(~grade)
 pb4
 
 
-
-#ggplot(df3,aes(x=loan_amnt)) + geom_histogram(aes(color= grade, fill = grade), 
-#                                              position="identity", bins =7) + 
-#  theme_minimal() +  
-#  theme(legend.position = "right", legend.title = element_blank(), 
-#        plot.title = element_text(hjust = 0.5),
-#        plot.subtitle = element_text(hjust = 0.5)) +
-#  labs(x = "Préstamo", 
-#       y = "", 
-#       title = "Monto del préstamo para diferentes grados de crédito",
-#       caption = "Fuente: elaboración propia") +
-#  scale_fill_manual(values=c(colores2[1], colores2[2], colores2[3],
-#                             colores2[4], colores2[5], colores2[6], colores2[8]))+
-#  scale_color_manual(values=c(colores2[1], colores2[2], colores2[3],
-#                              colores2[4], colores2[5], colores2[6], colores2[8]))
+## Gráfico 2 ##
 
 
-# nuevo 
+ggplot(dfs, aes(gdp_per_capita,Electricity_consumption_per_capita,color=Country)) + geom_point() + stat_smooth(method=lm)
 
+
+
+## Gráfico 3 ##
+
+df_fb <- read.csv("data/FB.csv")
+df_fb$Date <- as.Date(df_fb$Date)
+
+
+df_fb$Month <- strftime(df_fb$Date,"%m")
+df_fb$Month <- as.numeric(df_fb$Month)
+ggplot(df_fb, aes(Month,Close)) + 
+  geom_point(color="red",alpha=1/2,position = position_jitter(h=0.0,w=0.0))+
+  geom_line(stat='summary',fun.y=mean, color="blue",size=1)+
+  scale_x_continuous(breaks=seq(0,13,1))+
+  ggtitle("Monthly Closing Stock Prices: Facebook")+theme_classic()
+
+
+#### Gráficos corregidos ### 
+
+
+## Gráfico 1 ##
+
+# Juntamos todos los histogramas en un solo gráfico. Decidimos que cada barra tenga un color un poco 
+# transparente para así poder comparar mejor entre categorías. 
 
 ggplot(df3, aes(x = loan_amnt, fill = grade)) + 
   labs(x = "Préstamo", 
@@ -75,71 +87,20 @@ ggplot(df3, aes(x = loan_amnt, fill = grade)) +
   guides(fill=guide_legend(title="Grados"))
 
 
+# Guardamos el gráfico como jpg para que se mantenga la transparencia de los colores: 
 
-
+jpeg("primergrafico.jpeg", quality = 85)
 
 ## Gráfico 2 ##
 
-# original 
-
-ggplot(dfs, aes(gdp_per_capita,Electricity_consumption_per_capita,color=Country)) + geom_point() + stat_smooth(method=lm)
-
-
-
-# nuevo 
-
-# Hacemos zoom en uno de los gráficos realizados en clase 
-
+# Cargamos las librería para poder realizar zoom en los gráficos 
 
 library(ggforce)
 library(tidyverse)
 
-
+# Graficamos pero haciendo zoom sobre dos países:
 
 ggplot(dfs, aes(gdp_per_capita,Electricity_consumption_per_capita,color=Country)) + scale_y_log10()+
-  geom_point() + labs(x = "PBI per cápita", 
-                      y = "Consumo de electricidad per cápita", 
-                      title = "Relación entre consumo de electricidad y PBI",
-                      caption = "Fuente: elaboración propia")+ stat_smooth(method=lm)+facet_zoom(xlim = c(1000, 10000)) + theme_bw() +
-  theme(legend.position = "right", legend.title = element_blank(), 
-            plot.title = element_text(hjust = 0.5)) 
-
-
-
-
-
-
-## Gráfico 3 ##
-
-# original 
-
-
-
-#Read the data
-df_fb <- read.csv("data/FB.csv")
-df_fb$Date <- as.Date(df_fb$Date)
-#Use strftime to get the month for each date
-df_fb$Month <- strftime(df_fb$Date,"%m")
-df_fb$Month <- as.numeric(df_fb$Month)
-ggplot(df_fb, aes(Month,Close)) + 
-  geom_point(color="red",alpha=1/2,position = position_jitter(h=0.0,w=0.0))+
-  geom_line(stat='summary',fun.y=mean, color="blue",size=1)+
-  scale_x_continuous(breaks=seq(0,13,1))+
-  ggtitle("Monthly Closing Stock Prices: Facebook")+theme_classic()
-
-
-# nuevo creamos una variable para año 
-
-df_fb$Year <- strftime(df_fb$Date,"%y")
-
-# Creamos una variable que tenga el año completo: 
-
-df_fb$Año[df_fb$Year==17]<-"2017"
-
-df_fb$Año[df_fb$Year==18]<-"2018"
-
-
-ggplot(df_fb, aes(Month,Close,color=Año)) + scale_y_log10()+
   geom_point() + labs(x = "PBI per cápita", 
                       y = "Consumo de electricidad per cápita", 
                       title = "Relación entre consumo de electricidad y PBI",
@@ -148,19 +109,31 @@ ggplot(df_fb, aes(Month,Close,color=Año)) + scale_y_log10()+
         plot.title = element_text(hjust = 0.5)) 
 
 
-
-
-
-
-#### Gráficos corregidos ### 
-
-
-## Gráfico 1 ##
-
-
-
-## Gráfico 2 ##
-
-
-
 ## Gráfico 3 ##
+
+
+# Primero creamos una variable que tenga solo el año
+
+df_fb$Year <- strftime(df_fb$Date,"%y")
+
+# Ahora, creamos una variable que tenga el año completo (dado que la anterior solo tiene un número 
+# de dos cifras): 
+
+df_fb$Año[df_fb$Year==17]<-"2017"
+
+df_fb$Año[df_fb$Year==18]<-"2018"
+
+# A continuación graficamos un scatter plot, pero diferenciando según los años. También 
+# agregamos la media del precio de cierre de Facebook para cada año: 
+
+ggplot(df_fb, aes(Month,Close,color=Año)) + scale_y_log10()+
+  geom_point() +labs(x = "Mes", 
+                     y = "Precios de cierre (USD)", 
+                     title = "Precios de cierre mensuales: Facebook",
+                     caption = "Fuente: elaboración propia") +
+  scale_x_continuous(breaks=seq(0,13,1)) +
+  geom_line(stat='summary',fun.y=mean, color=colores2[6], size=1) + 
+  theme_minimal()  
+
+
+
